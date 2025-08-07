e63a95ed12f7e5400fc9af29ae13be30dae7c1ee
drhazem <166911324+smartclin@users.noreply.github.com>
2025-08-07 20:45:56 +0000

    update prisma

--- prisma/migrations/20250805042121_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Chat" ALTER COLUMN "userId" DROP NOT NULL;
--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ArrowRight, HeartPulse } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
